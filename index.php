3c77eb6f9a74cec341709742650818c0456ad544
pablohgomes1 <pabloh.gomes108@gmail.com>
2026-07-20 22:51:36 +0000

    feat: cria votacao basica com ranking

--- index.php
+++ index.php
@@ -56,7 +56,8 @@ $mensagem = $mensagens[$status] ?? '';
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
+
+<meta charset="UTF-8">
 <meta name="viewport"
 content="width=device-width, initial-scale=1.0">
 <title>Votação com PHP e Redis</title>
